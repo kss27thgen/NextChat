42a342c25f3615cc8fdecc167d96c1e34b8fb5a2
Oishi Yuikyo <mahayana3pm@gmail.com>
2022-01-02 19:33:59 +0900

    20220102 add avatar upload to auth

--- pages/auth.js
+++ pages/auth.js
@@ -1,13 +1,18 @@
 import React, { useContext, useEffect, useState } from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faEye } from "@fortawesome/free-solid-svg-icons";
-import { auth } from "../firebase";
+import {
+	faEye,
+	faUser,
+	faTimes,
+	faUserPlus,
+} from "@fortawesome/free-solid-svg-icons";
+import { auth, storage } from "../firebase";
 import {
 	createUserWithEmailAndPassword,
 	signInWithEmailAndPassword,
-	updateCurrentUser,
 	updateProfile,
 } from "firebase/auth";
+import { getDownloadURL, ref, uploadBytes } from "firebase/storage";
 import Router from "next/router";
 import AuthContext from "../context/auth/AuthContext";
 
@@ -17,6 +22,10 @@ const initialFormData = {
 	password: "",
 	passwordConf: "",
 };
+const initialFileData = {
+	fileObject: null,
+	fileName: "",
+};
 
 const Auth = () => {
 	const authContext = useContext(AuthContext);
@@ -24,29 +33,56 @@ const Auth = () => {
 
 	const [isLogin, setIsLogin] = useState(true);
 	const [formData, setFormData] = useState(initialFormData);
+	const [fileData, setFileData] = useState(initialFileData);
 	const [showPassword, setShowPassword] = useState(false);
 
 	const { name, email, password, passwordConf } = formData;
+	const { fileObject, fileName } = fileData;
 
 	const handleChange = (e) => {
-		setFormData({ ...formData, [e.target.name]: e.target.value });
+		if (e.target.name === "fileData" && e.target.files[0]) {
+			setFileData({
+				...fileData,
+				fileObject: e.target.files[0],
+				fileName: e.target.files[0].name,
+			});
+		} else {
+			setFormData({ ...formData, [e.target.name]: e.target.value });
+		}
+	};
+
+	const setAvatarUrl = async () => {
+		const storageRef = ref(storage, `avatars/${fileName}`);
+		await uploadBytes(storageRef, fileObject);
+		return getDownloadURL(ref(storage, `avatars/${fileName}`));
 	};
 
 	const register = async () => {
+		let avatarUrl =
+			"https://api-private.atlassian.com/users/8f525203adb5093c5954b43a5b6420c2/avatar";
+
 		try {
+			if (fileObject) {
+				avatarUrl = await setAvatarUrl();
+			}
+
+			// SignUp on firebase/auth
 			const res = await createUserWithEmailAndPassword(
 				auth,
 				email,
 				password,
 			);
+			// update profile on firebase/auth
 			await updateProfile(res.user, {
 				displayName: name,
-				photoURL: "https://example.com/jane-q-user/profile.jpg",
+				photoURL: avatarUrl,
 			});
+			// update current-user state
 			updateCurrentUser({
 				name,
-				photoUrl: "https://example.com/jane-q-user/profile.jpg",
+				photoUrl: avatarUrl,
 			});
+
 			Router.push("/");
 		} catch (error) {
 			alert(error.message);
@@ -82,15 +118,62 @@ const Auth = () => {
 					<h2>{isLogin ? "Login" : "Register"}</h2>
 					<form className="form" onSubmit={handleSubmit}>
 						{!isLogin && (
-							<div className="form--item">
-								<input
-									type="text"
-									placeholder="Name"
-									name="name"
-									value={name}
-									onChange={handleChange}
-								/>
-							</div>
+							<>
+								<div className="form--item">
+									<div className="avatar">
+										<label htmlFor="fileData">
+											<FontAwesomeIcon
+												icon={faUserPlus}
+											/>
+										</label>
+										<div>
+											{fileName ? (
+												<>
+													<div className="filename">
+														<span>
+															{fileName.length >
+															30
+																? fileName.substr(
+																		1,
+																		30,
+																  ) + "..."
+																: fileName}
+														</span>
+														<FontAwesomeIcon
+															icon={faTimes}
+															onClick={() =>
+																setFileData(
+																	initialFileData,
+																)
+															}
+														/>
+													</div>
+												</>
+											) : (
+												<label htmlFor="fileData">
+													AVATAR
+												</label>
+											)}
+										</div>
+										<input
+											style={{ display: "none" }}
+											type="file"
+											name="fileData"
+											onChange={handleChange}
+											id="fileData"
+										/>
+									</div>
+								</div>
+								<div className="form--item">
+									<input
+										type="text"
+										placeholder="Name"
+										name="name"
+										value={name}
+										onChange={handleChange}
+									/>
+								</div>
+							</>
 						)}
 						<div className="form--item">
 							<input
