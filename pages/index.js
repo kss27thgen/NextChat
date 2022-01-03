538dff7ac6e4cf15f3470da6e404a4ad251e8827
Oishi Yuikyo <mahayana3pm@gmail.com>
2022-01-03 20:22:13 +0900

    20220103 index side-bar styling

--- pages/index.js
+++ pages/index.js
@@ -1,31 +1,43 @@
+import { faArrowCircleLeft } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { signOut } from "firebase/auth";
 import Link from "next/link";
 import Router from "next/router";
+import { useState } from "react";
 import { auth } from "../firebase";
 
 const Home = () => {
+	const [onSidebar, setOnSidebar] = useState(true);
+
 	return (
 		<>
 			<div className="index">
-				<header>
-					<ul>
-						<li>
-							<Link href="/">Home</Link>
-						</li>
-						<li>
-							<Link href="/auth">Auth</Link>
-						</li>
-						<li
-							onClick={() => {
-								signOut(auth);
-								Router.push("/auth");
-							}}
-						>
-							signOut
-						</li>
-					</ul>
-				</header>
-				<main></main>
+				<div className={`${onSidebar ? "on" : "off"} index--wrapper`}>
+					<aside>
+						<ul>
+							<li>
+								<Link href="/">Home</Link>
+							</li>
+							<li>
+								<Link href="/auth">Auth</Link>
+							</li>
+							<li
+								onClick={() => {
+									signOut(auth);
+									Router.push("/auth");
+								}}
+							>
+								signOut
+							</li>
+						</ul>
+						<FontAwesomeIcon
+							className={onSidebar ? "on" : "off"}
+							icon={faArrowCircleLeft}
+							onClick={() => setOnSidebar(!onSidebar)}
+						/>
+					</aside>
+					<main>main</main>
+				</div>
 			</div>
 		</>
 	);
