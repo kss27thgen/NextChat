1347b366ebaa9f24143ce09b230bdce4fc374cae
Oishi Yuikyo <mahayana3pm@gmail.com>
2022-01-03 19:22:04 +0900

    202201103 media query setup

--- pages/index.js
+++ pages/index.js
@@ -6,25 +6,27 @@ import { auth } from "../firebase";
 const Home = () => {
 	return (
 		<>
-			<header>
-				<ul>
-					<li>
-						<Link href="/">Home</Link>
-					</li>
-					<li>
-						<Link href="/auth">Auth</Link>
-					</li>
-					<li
-						onClick={() => {
-							signOut(auth);
-							Router.push("/auth");
-						}}
-					>
-						signOut
-					</li>
-				</ul>
-			</header>
-			<main></main>
+			<div className="index">
+				<header>
+					<ul>
+						<li>
+							<Link href="/">Home</Link>
+						</li>
+						<li>
+							<Link href="/auth">Auth</Link>
+						</li>
+						<li
+							onClick={() => {
+								signOut(auth);
+								Router.push("/auth");
+							}}
+						>
+							signOut
+						</li>
+					</ul>
+				</header>
+				<main></main>
+			</div>
 		</>
 	);
 };
